505500415b2b0c0768efefb2bc66dca4b02f3e71
jaeyeoneej <5ikve@naver.com>
2023-02-08 22:26:40 +0900

    prac: #0 dragConstraints 적용 23-02-08
    - constraints: 제약하는 것
    - box가 일정 영역을 벗어나지 못하게 공간을 제한함

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,17 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
@@ -20,19 +31,20 @@ const Box = styled(motion.div)`
 const boxVariants: Variants = {
   hover: { scale: 1.5, rotateZ: 90 },
   click: { scale: 1, borderRadius: "100px" },
-  drag: { backgroundColor: "rgb(46,204,113)", transition: { duration: 10 } },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={boxVariants}
-        whileDrag="drag"
-        whileHover="hover"
-        whileTap="click"
-      />
+      <BiggerBox>
+        <Box
+          drag
+          dragConstraints={{ top: -200, bottom: 200, left: -200, right: 200 }}
+          variants={boxVariants}
+          whileHover="hover"
+          whileTap="click"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
