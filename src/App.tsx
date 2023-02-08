df24fc782186f7d99dfabdaed962c46e52514efe
jaeyeoneej <5ikve@naver.com>
2023-02-08 20:55:37 +0900

    prac: #0 variants로 stage 분리 23-02-08
    - variants를 사용하면 코드를 보다 깔끔하게 만들 수 있음
    - variants를 사용하여 원하는 만큼 많은 stage를 설정해줄 수 있음
    - 연습에서는 start로 initial 역할을, end로 animate(finished) 역할을 하게 함

--- src/App.tsx
+++ src/App.tsx
@@ -17,14 +17,19 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVars = {
+  start: { scale: 0 },
+  end: {
+    scale: 1,
+    rotateZ: "360deg",
+    transition: { type: "spring", bounce: 0.5, delay: 0.5 },
+  },
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box
-        transition={{ type: "spring", bounce: 0.5, delay: 0.5 }}
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: "360deg" }}
-      />
+      <Box variants={myVars} initial="start" animate="end" />
     </Wrapper>
   );
 }
