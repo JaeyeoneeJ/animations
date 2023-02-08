d11da5a2c6442f17fbea54ac6dd43ed9ef69b793
jaeyeoneej <5ikve@naver.com>
2023-02-08 22:10:11 +0900

    prac: #0 variants 적용 23-02-08
    - typescript로 자동완성을 위해 variants를 import 하여 boxVariants에 타입 지정함

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -17,15 +17,15 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {};
+const boxVariants: Variants = {
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "100px" },
+};
 
 function App() {
   return (
     <Wrapper>
-      <Box
-        whileHover={{ scale: 1.5, rotateZ: 90 }}
-        whileTap={{ scale: 1, borderRadius: "100px" }}
-      />
+      <Box variants={boxVariants} whileHover="hover" whileTap="click" />
     </Wrapper>
   );
 }
