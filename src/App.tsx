bb61e030ef5f1ac6ee9826e4e1cae38edb22f4a1
jaeyeoneej <5ikve@naver.com>
2023-02-08 20:37:15 +0900

    test: #0 Box 컴포넌트 animate props 테스트 23-02-08
    - animate 된 styled-component를 가지기 위해 기존 styled.div 형식으로 생성하지 않음
    - styled(motion.div)로 생성
    - => 기존 styled 컴포넌트의 스타일을 복제하기 위해 했던 형태를 쓰는 것임

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,18 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box />
-      <motion.div></motion.div>
+      <Box transition={{ duration: 3 }} animate={{ borderRadius: "100px" }} />
     </Wrapper>
   );
 }
