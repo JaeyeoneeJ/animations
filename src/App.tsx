ddd6a08eca175a9cbd1ce93ffd9ed24d8213e9b3
jaeyeoneej <5ikve@naver.com>
2023-02-08 20:25:35 +0900

    structure: #0 폴더 구조 작업 시작 23-02-08

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <Box />
+    </Wrapper>
+  );
+}
+
+export default App;
---
+++ src/styled.d.ts
@@ -0,0 +1,6 @@
+import "styled-components";
+
+// and extend them!
+declare module "styled-components" {
+  export interface DefaultTheme {}
+}
