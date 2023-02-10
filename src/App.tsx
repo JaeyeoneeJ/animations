3d69b64c9cfd4bcafa6bc9d1cc88dde368e7274a
jaeyeoneej <5ikve@naver.com>
2023-02-10 16:50:02 +0900

    prac: #0 slider 이전 버튼 클릭 시 animation 변경 23-02-10
    - 기존에 slider animate를 구현하려면 map으로 만든 후, null로 일치하지 않는 객체를 보이지 않게 했었음
    - key는 element의 고유 값을 식별하기 위함이므로 key가 변경되면 해당 컴포넌트는 re-rendering 됨
    => react.js가 새 component가 생겼다고 생각하게 되는 것임
    
    - custom 속성을 통해 variants를 변경할 수 있음
    => 단, variants는 함수여야만 함
    
    - 함수 조건문으로 custom의 상태에 따라 x 값을 반대로 설정하여 animation 해결

--- src/App.tsx
+++ src/App.tsx
@@ -27,12 +27,12 @@ const Box = styled(motion.div)`
 `;
 
 const box = {
-  invisible: {
-    x: 500,
+  entry: (isBack: boolean) => ({
+    x: isBack ? -500 : 500,
     opacity: 0,
     scale: 0,
-  },
-  visible: {
+  }),
+  center: {
     x: 0,
     opacity: 1,
     scale: 1,
@@ -40,36 +40,40 @@ const box = {
       duration: 1,
     },
   },
-  exit: {
-    x: -500,
+  exit: (isBack: boolean) => ({
+    x: isBack ? 500 : -500,
     opacity: 0,
-    rotateX: 180,
+    scale: 0,
     transition: {
       duration: 1,
     },
-  },
+  }),
 };
 
 function App() {
   const [visible, setVisivle] = useState(1);
-  const nextPlease = () => setVisivle((prev) => (prev === 10 ? 10 : prev + 1));
-  const prevPlease = () => setVisivle((prev) => (prev === 1 ? 1 : prev - 1));
+  const [back, setBack] = useState(false);
+  const nextPlease = () => {
+    setBack(false);
+    setVisivle((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPlease = () => {
+    setBack(true);
+    setVisivle((prev) => (prev === 1 ? 1 : prev - 1));
+  };
   return (
     <Wrapper>
-      <AnimatePresence>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
-          i === visible ? (
-            <Box
-              variants={box}
-              initial="invisible"
-              animate="visible"
-              exit="exit"
-              key={i}
-            >
-              {i}
-            </Box>
-          ) : null
-        )}
+      <AnimatePresence custom={back}>
+        <Box
+          custom={back}
+          variants={box}
+          initial="entry"
+          animate="center"
+          exit="exit"
+          key={visible}
+        >
+          {visible}
+        </Box>
       </AnimatePresence>
       <button onClick={nextPlease}>next</button>
       <button onClick={prevPlease}>prev</button>
