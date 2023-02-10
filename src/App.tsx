cd5e91003003465eaf12fef3291618d57c4c7f5c
jaeyeoneej <5ikve@naver.com>
2023-02-10 18:58:17 +0900

    fixed: #0 해당 카드를 클릭할 때 layoutId로 연결하여 animation 구현 23-02-10
    - Grid 내 Box 마다 다른 key와 layoutId를 가지고 있음
    - Box 클릭 시 Id 값을 다르게 주어 Overlay에 있는 Box의 layoutId와 연동시킴
    - 각기 다른 Box를 눌러도 animate가 정상적으로 실행되는 것을 확인

--- src/App.tsx
+++ src/App.tsx
@@ -43,24 +43,23 @@ const Overlay = styled(motion.div)`
 `;
 
 function App() {
-  const [clicked, setClicked] = useState(false);
-  const toggle = () => setClicked((prev) => !prev);
+  const [id, setId] = useState<null | string>(null);
   return (
-    <Wrapper onClick={toggle}>
+    <Wrapper>
       <Grid>
-        <Box layoutId="hello" />
-        <Box />
-        <Box />
-        <Box />
+        {["1", "2", "3", "4"].map((n) => (
+          <Box onClick={() => setId(n)} key={n} layoutId={n} />
+        ))}
       </Grid>
       <AnimatePresence>
-        {clicked ? (
+        {id ? (
           <Overlay
+            onClick={() => setId(null)}
             initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
             animate={{ backgroundColor: "rgba(0, 0, 0, 0.7)" }}
             exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
           >
-            <Box layoutId="hello" style={{ width: 400, height: 200 }} />
+            <Box layoutId={id} style={{ width: 400, height: 200 }} />
           </Overlay>
         ) : null}
       </AnimatePresence>
