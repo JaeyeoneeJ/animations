5be2b3ee8404f9d224f9d33a44eb620ab5f6166f
jaeyeoneej <5ikve@naver.com>
2023-02-10 16:14:16 +0900

    prac: #0 슬라이더 구현 23-02-10
    - Box 내용을 useState와 연결하여 작성
    - 내부 animate를 자유롭게 만들 수 있어 유용함

--- src/App.tsx
+++ src/App.tsx
@@ -9,63 +9,68 @@ const Wrapper = styled(motion.div)`
   justify-content: center;
   align-items: center;
   background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
+  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
-  position: absolute;
-  top: 100px;
   width: 200px;
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  box-shadow: 0 2px 3px rgba(101, 61, 61, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
+  position: absolute;
+  top: 100px;
 `;
 
-const boxVariants = {
-  initial: {
+const box = {
+  invisible: {
+    x: 500,
     opacity: 0,
     scale: 0,
   },
   visible: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
+    transition: {
+      duration: 1,
+    },
   },
-  leaving: {
+  exit: {
+    x: -500,
     opacity: 0,
-    y: 50,
-    scale: 0.8,
+    rotateX: 180,
+    transition: {
+      duration: 1,
+    },
   },
 };
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const [visible, setVisivle] = useState(1);
+  const nextPlease = () => setVisivle((prev) => (prev === 10 ? 10 : prev + 1));
   return (
     <Wrapper>
-      <button
-        style={{
-          border: "2px solid white",
-          backgroundColor: "transparent",
-          color: "white",
-          borderRadius: "10px",
-          padding: "4px 8px",
-          cursor: "pointer",
-        }}
-        onClick={toggleShowing}
-      >
-        Click
-      </button>
       <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
-        ) : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+          i === visible ? (
+            <Box
+              variants={box}
+              initial="invisible"
+              animate="visible"
+              exit="exit"
+              key={i}
+            >
+              {i}
+            </Box>
+          ) : null
+        )}
       </AnimatePresence>
+      <button onClick={nextPlease}>next</button>
     </Wrapper>
   );
 }
